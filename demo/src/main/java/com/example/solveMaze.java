c1de669983159d6bc88166fc380f3459afedff9b
AriqFaridzki <ariqf35@gmail.com>
2024-06-27 22:00:11 +0700

    init dan lagi nyari solusi

--- demo/src/main/java/com/example/solveMaze.java
+++ demo/src/main/java/com/example/solveMaze.java
@@ -14,7 +14,8 @@ public class solveMaze {
         this.titikAwal = titikAwal;
         this.titikAkhir = titikAkhir;
 
-        int[][] mazeHistory = new int[mazeRow][mazeCol];
+        int[][] mazeHistoryDist = new int[mazeRow][mazeCol];
+        Koordinat[][] mazeHistory = new Koordinat[mazeRow][mazeCol];
 
         Queue<Koordinat> queue = new LinkedList<>();
         queue.offer(this.titikAwal);
@@ -25,6 +26,8 @@ public class solveMaze {
 
         while(!queue.isEmpty()) {
             titikSekarang = queue.poll();
+            mazeHistoryDist[titikSekarang.getRow()][titikSekarang.getCol()] = titikSekarang.getDistance();
+            mazeHistory[titikSekarang.getRow()][titikSekarang.getCol()] = titikSekarang;
             int titikAkhirRow = this.titikAkhir.getRow();
             int titikAkhirCol = this.titikAkhir.getCol();
 
@@ -35,6 +38,7 @@ public class solveMaze {
 
             if(titikSekarang.getRow() == titikAkhirRow) {
                 if (titikSekarang.getCol() == titikAkhirCol) {
+                    printMazePath(maze,mazeHistory, titikSekarang);
                     return titikSekarang.getDistance();
                 }
             }
@@ -101,6 +105,7 @@ public class solveMaze {
         return 0;
     }
 
+
     public void printVisitedPoint(boolean[][] booleanMatrix){
         for (boolean[] matrix : booleanMatrix) {
             System.out.print("|");
@@ -119,14 +124,70 @@ public class solveMaze {
         }
     }
 
-    public void printMazePath(int[][] mazeHistory){
-        System.out.println("Maze history : ");
+    public void printMazePath(String[][] maze, Koordinat[][] mazeHistory, Koordinat destination){
+        // destination 0,4, gimana caranya ke 9,0?
+        // we have mazeHistory which is the total distance taken from 9,0
+        // each row have unique number of distances
 
-        for (int i = 0; i < mazeHistory.length; i++) {
+        //        Maze history :
+        //        Row 0 - 9|0|11|0|13| <-- start here
+//                Row 1 - 8|0|10|0|12| <
+//                Row 2 - 7|0|9|0|11|
+//                Row 3 - 6|0|8|0|10|
+//                Row 4 - 5|0|7|0|9|
+//                Row 5 - 4|5|6|7|8|
+//                Row 6 - 3|0|5|0|7|
+//                Row 7 - 2|3|4|5|6|
+//                Row 8 - 1|2|0|6|7|
+//                Row 9 - 0|0|0|0|0|
+
+        // String[][] maze = {
+        //            {" ", "#", " ", "#", " "},
+        //            {" ", "#", " ", "#", " "},
+        //            {" ", "#", " ", "#", " "},
+        //            {" ", "#", " ", "#", " "},
+        //            {" ", "#", " ", "#", " "},
+        //            {" ", " ", " ", " ", " "},
+        //            {" ", "#", " ", "#", " "},
+        //            {" ", " ", " ", " ", " "},
+        //            {" ", " ", "#", " ", " "},
+        //            {" ", "#", "#", "#", "#"}
+        //        };
+
+        // 1st loop check Row
+        // 2nd Loop check Column
+        //
+
+        String[][] fixedPath = new String[maze.length][maze[0].length];
+        int currentRow = destination.getRow(); // 0
+        int currentCol= destination.getCol(); // 4
+        Koordinat next;
+
+        while(currentRow != -1 && currentCol != -1){
+            fixedPath[currentRow][currentCol] = "✨"; // tandai 0,4 dengan bintang
+
+            next = mazeHistory[currentRow][currentCol]; // akses mazeHistory dengan cords 0,4 yaitu sebuah koordinat
+
+            if(next != null){ // jika koordinat yang diakses itu ndak null maka > ambil valuenya
+                currentRow = next.getRow();
+                currentCol = next.getCol();
+            } else { // terminate loops
+                currentRow = -1;
+                currentCol = -1;
+            }
+        }
+
+//        System.out.println("Maze history : ");
+
+//        for (String[] path : fixedPath){
+//
+//        }
+//
+        for (int i = 0; i < fixedPath.length; i++) {
             System.out.print("Row " + i + " - ");
 
-            for (int j = 0; j < mazeHistory[i].length; j++) {
-                System.out.print(mazeHistory[i][j] + "|");
+            for (int j = 0; j < fixedPath[i].length; j++) {
+                System.out.print(fixedPath[i][j] + "|");
             }
 
             System.out.println();
